16c38974c699aa2c985f82f44db56a3bcb61fd99
Helza Aragão <helza.batista.aragao@gmail.com>
2026-01-12 18:22:26 -0300

    +Sequelize

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,14 @@
 import { fastify } from 'fastify'
 import { fastifyCors } from '@fastify/cors'
+import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod'
 
 const app = fastify()
 
-app.register(fastifyCors, {origin: '*'})
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(fastifyCors, {origin: '*'}) 
 
 app.listen({port: 7777}).then(() => {
     console.log('HTTP server running!')
-})
+}) 
